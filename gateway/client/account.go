49a2697dfd4712debcf0da19814afa0d5ac590a2
Umberman <f.kayumov97@gmail.com>
2023-08-24 17:51:32 +0500

    minor changes

--- gateway/client/account.go
+++ gateway/client/account.go
@@ -4,8 +4,24 @@ import (
 	"fmt"
 
 	"github.com/Feruz666/serve/gateway/config"
+	account "github.com/Feruz666/serve/gateway/protos/account/protos"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"google.golang.org/protobuf/proto"
 )
+
+type KafkaProducer struct {
+	producer   *kafka.Producer
+	topic      string
+	deliveryCh chan kafka.Event
+}
+
+func NewKafkaProducer(p *kafka.Producer, topic string) *KafkaProducer {
+	return &KafkaProducer{
+		producer:   p,
+		topic:      topic,
+		deliveryCh: make(chan kafka.Event, 1000),
+	}
+}
 
 // AccountClient client for account service
 type AccountClient struct {
@@ -19,15 +35,43 @@ func NewAccountClient(config *config.Config) *AccountClient {
 }
 
 func (c *AccountClient) ConnectToKafka() (*kafka.Producer, error) {
-	p, err := kafka.NewProducer(&kafka.ConfigMap{
+	p := NewKafkaProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
 		"acks":              "all",
 	})
 
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to connect to kafka: %w", err)
+		return nil, fmt.Errorf("failed to connect to kafka: %w", err)
 	}
 
 	return p, nil
+}
 
+func (c AccountClient) CreatePerson() error {
+	p, err := c.ConnectToKafka()
+
+	if err != nil {
+		fmt.Errorf("c.ConnectToKafka failed: %w", err)
+	}
+
+	topic := "account-topic"
+	pers := &account.Person{
+		Name:  "asd",
+		Id:    223,
+		Email: "asd@gmail.com",
+	}
+
+	buff, err := proto.Marshal(pers)
+
+	err = p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: int32(kafka.PartitionAny),
+		},
+		Value: buff,
+	},
+		p.deliveryCh,
+	)
+
+	return nil
 }
